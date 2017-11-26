585fe2897e66e74fd9c457ae7c8d80d473e12ef2
Monika Baran <mobaran@student.agh.edu.pl>
2017-11-26 16:49:40 +0100

    added 6.3_m

--- lab6/zad6.3_m/src/DB.java
+++ lab6/zad6.3_m/src/DB.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DB {
 
@@ -6,30 +7,25 @@ public class DB {
     private Statement stmt = null;
     private ResultSet rs = null;
 
-    public void connect() {
+    public void connect() throws SQLException {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            //Class.forName("com.mysql.jdbc.Driver");
             conn =
-                    DriverManager.getConnection("jdbc:mysql://mysql.agh.edu.pl/mobaran",
+                    DriverManager.getConnection("jdbc:mysql://mysql.agh.edu.pl/mobaran?serverTimezone=Europe/Warsaw",
                             "mobaran", "9Zef6mNt3BETGyUV");
-        } catch (SQLException ex) {
-            // handle any errors
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void listNames() {
+    /*public void listNames() {
         try {
             connect();
             stmt = conn.createStatement();
 
             // Wyciagamy wszystkie pola z kolumny name
             // znajdujące się w tabeli users
-            rs = stmt.executeQuery("SELECT name FROM users");
+            rs = stmt.executeQuery("SELECT title FROM books");
 
             while (rs.next()) {
                 String name = rs.getString(1);
@@ -57,5 +53,62 @@ public class DB {
                 stmt = null;
             }
         }
+    }*/
+
+    public Book getBookByIsbn(Long isbn) {
+
+        Book book = new Book();
+
+        try {
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM books WHERE isbn ='" + isbn + "'");
+
+            while (rs.next()) {
+                book.setTitle(rs.getString(2));
+                book.setAuthor(rs.getString(3));
+                book.setYear(rs.getInt(4));
+                book.setIsbn(isbn);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return book;
     }
+
+    public ArrayList<Book> getBookByAuthor(String author) {
+
+        ArrayList<Book> bookList = new ArrayList<>();
+        Book book = new Book();
+
+        try {
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM books WHERE author ='" + author + "'");
+
+            while (rs.next()) {
+                book.setTitle(rs.getString(2));
+                book.setAuthor(author);
+                book.setYear(rs.getInt(4));
+                book.setIsbn(rs.getLong(1));
+
+                bookList.add(book);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return bookList;
+    }
+
+    public void deleteBookByIsbn(Long isbn) {
+
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate("DELETE FROM books WHERE isbn ='" + isbn + "'");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- lab6/zad6.3_m/src/ProgramDB.java
+++ lab6/zad6.3_m/src/ProgramDB.java
@@ -1,7 +1,45 @@
+import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
+
 public class ProgramDB {
     public static void main(String[] args) {
+
         DB db = new DB();
 
-        db.connect();
+        int connections = 0;
+        int noMoreConnections = 3;
+
+        while (connections < noMoreConnections) {
+            try {
+                db.connect();
+                connections = noMoreConnections;
+
+                //by isbn
+                Book book = db.getBookByIsbn(1234567891236L);
+                book.printBook();
+
+                //by author
+                System.out.println("\n\n__________________________________________________");
+                System.out.println("Search by author: ");
+                for (Book b : db.getBookByAuthor("Ernest Hemingway")){
+                    b.printBook();
+                }
+                System.out.println("\n\n__________________________________________________");
+
+                //deleting
+                db.deleteBookByIsbn(1234567891234L);
+                book = db.getBookByIsbn(1234567891234L);
+                book.printBook();
+
+            } catch (SQLException e) {
+                System.out.println("Couldn't connect to database. Retrying in 10s");
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException ex) {
+                    System.out.println("Timer interupted");
+                }
+                connections++;
+            }
+        }
     }
 }
